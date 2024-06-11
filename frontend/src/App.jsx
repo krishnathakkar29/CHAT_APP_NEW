60b7add2829813e8267f4bf894c1cab40c2f91c4
Krishna Thakkar <krishnathakkar1304@gmail.com>
2024-06-11 12:36:57 +0530

    intial setup and login page UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat/:chatId" element={<Chat />} />
+          <Route path="/groups" element={<Groups />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
