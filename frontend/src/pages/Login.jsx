fd35c562b64100badf562aaeb55c127b13f841dc
Krishna Thakkar <krishnathakkar1304@gmail.com>
2024-06-11 13:20:53 +0530

    UI Update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,262 @@
+// import React, { useState } from "react";
+// import {
+//   Avatar,
+//   Button,
+//   Container,
+//   IconButton,
+//   Paper,
+//   Stack,
+//   TextField,
+//   Typography,
+// } from "@mui/material";
+
+// import { useFileHandler, useInputValidation } from "6pp";
+// import { CameraAlt as CameraAltIcon } from "@mui/icons-material";
+// import { VisuallyHiddenInput } from "../components/styles/StyledComponents";
+// import { usernameValidator } from "../utils/validators";
+// import { bgGradient } from "../constant/color";
+// import ParticleRing from "../components/ParticleRing";
+
+// let user = true;
+// function Login() {
+//   const [isLogin, setIsLogin] = useState(true);
+
+//   const username = useInputValidation("", usernameValidator);
+//   const password = useInputValidation("");
+//   const name = useInputValidation("");
+//   const bio = useInputValidation("");
+
+//   const avatar = useFileHandler("single");
+
+//   const toggleLogin = () => setIsLogin((prev) => !prev);
+//   const handleLogin = (e) => {
+//     e.preventDefault();
+//   };
+//   const handleSignUp = (e) => {
+//     e.preventDefault();
+//   };
+//   return (
+//     <div
+//       style={{
+//         backgroundImage: bgGradient,
+//       }}
+//     >
+//       {/*  <div style={{ position: "relative", height: "100vh" }}> */}
+
+//       <Container
+//         component={"main"}
+//         maxWidth="xs"
+//         sx={{
+//           height: "100vh",
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <Paper
+//           elevation={3}
+//           sx={{
+//             padding: 4,
+//             display: "flex",
+//             flexDirection: "column",
+//             alignItems: "center",
+//           }}
+//         >
+//           {isLogin ? (
+//             <>
+//               <Typography variant="h5">Login</Typography>
+//               <form
+//                 style={{
+//                   width: "100%",
+//                   marginTop: "1rem",
+//                 }}
+//                 onSubmit={handleLogin}
+//               >
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   label="Username"
+//                   margin="normal"
+//                   variant="outlined"
+//                   value={username.value}
+//                   onChange={username.changeHandler}
+//                 />
+
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   label="Password"
+//                   type="password"
+//                   margin="normal"
+//                   variant="outlined"
+//                   value={password.value}
+//                   onChange={password.changeHandler}
+//                 />
+
+//                 <Button
+//                   sx={{
+//                     marginTop: "1rem",
+//                   }}
+//                   variant="contained"
+//                   color="primary"
+//                   type="submit"
+//                   fullWidth
+//                   //   disabled={isLoading}
+//                 >
+//                   Login
+//                 </Button>
+
+//                 <Typography textAlign={"center"} m={"1rem"}>
+//                   OR
+//                 </Typography>
+
+//                 <Button
+//                   //   disabled={isLoading}
+//                   fullWidth
+//                   variant="text"
+//                   onClick={toggleLogin}
+//                 >
+//                   Sign Up Instead
+//                 </Button>
+//               </form>
+//             </>
+//           ) : (
+//             <>
+//               <Typography variant="h5">Sign Up</Typography>
+//               <form
+//                 style={{
+//                   width: "100%",
+//                   marginTop: "1rem",
+//                 }}
+//                 onSubmit={handleSignUp}
+//               >
+//                 <Stack position={"relative"} width={"10rem"} margin={"auto"}>
+//                   <Avatar
+//                     sx={{
+//                       width: "10rem",
+//                       height: "10rem",
+//                       objectFit: "contain",
+//                     }}
+//                     // src={avatar.preview}
+//                   />
+
+//                   <IconButton
+//                     sx={{
+//                       position: "absolute",
+//                       bottom: "0",
+//                       right: "0",
+//                       color: "white",
+//                       bgcolor: "rgba(0,0,0,0.5)",
+//                       ":hover": {
+//                         bgcolor: "rgba(0,0,0,0.7)",
+//                       },
+//                     }}
+//                     component="label"
+//                   >
+//                     <>
+//                       <CameraAltIcon />
+//                       <VisuallyHiddenInput
+//                         type="file"
+//                         onChange={avatar.changeHandler}
+//                       />
+//                     </>
+//                   </IconButton>
+//                 </Stack>
+
+//                 {avatar.error && (
+//                   <Typography
+//                     m={"1rem auto"}
+//                     width={"fit-content"}
+//                     display={"block"}
+//                     color="error"
+//                     variant="caption"
+//                   >
+//                     {avatar.error}
+//                   </Typography>
+//                 )}
+
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   label="Name"
+//                   margin="normal"
+//                   variant="outlined"
+//                   value={name.value}
+//                   onChange={name.changeHandler}
+//                 />
+
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   label="Bio"
+//                   margin="normal"
+//                   variant="outlined"
+//                   value={bio.value}
+//                   onChange={bio.changeHandler}
+//                 />
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   label="Username"
+//                   margin="normal"
+//                   variant="outlined"
+//                   value={username.value}
+//                   onChange={username.changeHandler}
+//                 />
+
+//                 {username.error && (
+//                   <Typography color="error" variant="caption">
+//                     {username.error}
+//                   </Typography>
+//                 )}
+
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   label="Password"
+//                   type="password"
+//                   margin="normal"
+//                   variant="outlined"
+//                   value={password.value}
+//                   onChange={password.changeHandler}
+//                 />
+
+//                 <Button
+//                   sx={{
+//                     marginTop: "1rem",
+//                   }}
+//                   variant="contained"
+//                   color="primary"
+//                   type="submit"
+//                   fullWidth
+//                   //   disabled={isLoading}
+//                 >
+//                   Sign Up
+//                 </Button>
+
+//                 <Typography textAlign={"center"} m={"1rem"}>
+//                   OR
+//                 </Typography>
+
+//                 <Button
+//                   //   disabled={isLoading}
+//                   fullWidth
+//                   variant="text"
+//                   onClick={toggleLogin}
+//                 >
+//                   Login Instead
+//                 </Button>
+//               </form>
+//             </>
+//           )}
+//         </Paper>
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
 import React, { useState } from "react";
 import {
   Avatar,
@@ -9,13 +268,13 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-
 import { useFileHandler, useInputValidation } from "6pp";
 import { CameraAlt as CameraAltIcon } from "@mui/icons-material";
 import { VisuallyHiddenInput } from "../components/styles/StyledComponents";
 import { usernameValidator } from "../utils/validators";
-import { bgGradient } from "../constant/color";
-function Login() {
+import ParticleRing from "../components/ParticleRing";
+
+const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
 
   const username = useInputValidation("", usernameValidator);
@@ -32,14 +291,17 @@ function Login() {
   const handleSignUp = (e) => {
     e.preventDefault();
   };
+
   return (
     <div
-      style={
-        {
-          backgroundImage: bgGradient,
-        }
-      }
+      style={{
+        position: "relative",
+        height: "100vh",
+        overflow: "hidden",
+        // backgroundColor: "#1E293B",
+      }}
     >
+      <ParticleRing />
       <Container
         component={"main"}
         maxWidth="xs"
@@ -51,17 +313,37 @@ function Login() {
         }}
       >
         <Paper
+          // elevation={3}
+          // sx={{
+          //   padding: 4,
+          //   display: "flex",
+          //   flexDirection: "column",
+          //   alignItems: "center",
+          //   zIndex: 1,
+          //   position: "relative",
+          //   backdropFilter: "blur(10px)",
+          //   backgroundColor: "rgba(30, 41, 59, 0.8)", // Darker background color
+          //   color: "#E2E8F0", // Light text color
+          // }}
           elevation={3}
           sx={{
             padding: 4,
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
+            zIndex: 1,
+            position: "relative",
+            backdropFilter: "blur(10px)",
+            backgroundColor: "#274472", // Adjust opacity for a lighter color
+            color: "#E2E8F0", // Light text color
+            boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)", // Add box shadow
           }}
         >
           {isLogin ? (
             <>
-              <Typography variant="h5">Login</Typography>
+              <Typography variant="h5" sx={{ color: "#E2E8F0" }}>
+                Login
+              </Typography>
               <form
                 style={{
                   width: "100%",
@@ -77,6 +359,17 @@ function Login() {
                   variant="outlined"
                   value={username.value}
                   onChange={username.changeHandler}
+                  InputLabelProps={{
+                    style: { color: "#94A3B8" }, // Label color
+                  }}
+                  InputProps={{
+                    style: { color: "#E2E8F0" }, // Text color
+                    sx: {
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#6B7280", // Adjust to match the background color
+                      },
+                    },
+                  }}
                 />
 
                 <TextField
@@ -88,30 +381,49 @@ function Login() {
                   variant="outlined"
                   value={password.value}
                   onChange={password.changeHandler}
+                  InputLabelProps={{
+                    style: { color: "#94A3B8" },
+                  }}
+                  InputProps={{
+                    style: { color: "#E2E8F0" },
+                    sx: {
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#94A3B8",
+                      },
+                    },
+                  }}
                 />
 
                 <Button
                   sx={{
                     marginTop: "1rem",
+                    backgroundColor: "#4B5563", // Button background color
+                    color: "#E2E8F0", // Button text color
+                    "&:hover": {
+                      backgroundColor: "#2563EB",
+                    },
                   }}
                   variant="contained"
                   color="primary"
                   type="submit"
                   fullWidth
-                  //   disabled={isLoading}
                 >
                   Login
                 </Button>
 
-                <Typography textAlign={"center"} m={"1rem"}>
+                <Typography
+                  textAlign={"center"}
+                  m={"1rem"}
+                  sx={{ color: "#E2E8F0" }}
+                >
                   OR
                 </Typography>
 
                 <Button
-                  //   disabled={isLoading}
                   fullWidth
                   variant="text"
                   onClick={toggleLogin}
+                  sx={{ color: "#3B82F6" }} // Link color
                 >
                   Sign Up Instead
                 </Button>
@@ -119,7 +431,9 @@ function Login() {
             </>
           ) : (
             <>
-              <Typography variant="h5">Sign Up</Typography>
+              <Typography variant="h5" sx={{ color: "#E2E8F0" }}>
+                Sign Up
+              </Typography>
               <form
                 style={{
                   width: "100%",
@@ -133,8 +447,8 @@ function Login() {
                       width: "10rem",
                       height: "10rem",
                       objectFit: "contain",
+                      bgcolor: "#94A3B8", // Avatar background color
                     }}
-                    // src={avatar.preview}
                   />
 
                   <IconButton
@@ -160,7 +474,7 @@ function Login() {
                   </IconButton>
                 </Stack>
 
-                 {avatar.error && (
+                {avatar.error && (
                   <Typography
                     m={"1rem auto"}
                     width={"fit-content"}
@@ -180,6 +494,17 @@ function Login() {
                   variant="outlined"
                   value={name.value}
                   onChange={name.changeHandler}
+                  InputLabelProps={{
+                    style: { color: "#94A3B8" },
+                  }}
+                  InputProps={{
+                    style: { color: "#E2E8F0" },
+                    sx: {
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#94A3B8",
+                      },
+                    },
+                  }}
                 />
 
                 <TextField
@@ -190,6 +515,17 @@ function Login() {
                   variant="outlined"
                   value={bio.value}
                   onChange={bio.changeHandler}
+                  InputLabelProps={{
+                    style: { color: "#94A3B8" },
+                  }}
+                  InputProps={{
+                    style: { color: "#E2E8F0" },
+                    sx: {
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#94A3B8",
+                      },
+                    },
+                  }}
                 />
                 <TextField
                   required
@@ -199,6 +535,17 @@ function Login() {
                   variant="outlined"
                   value={username.value}
                   onChange={username.changeHandler}
+                  InputLabelProps={{
+                    style: { color: "#94A3B8" },
+                  }}
+                  InputProps={{
+                    style: { color: "#E2E8F0" },
+                    sx: {
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#94A3B8",
+                      },
+                    },
+                  }}
                 />
 
                 {username.error && (
@@ -216,30 +563,49 @@ function Login() {
                   variant="outlined"
                   value={password.value}
                   onChange={password.changeHandler}
+                  InputLabelProps={{
+                    style: { color: "#94A3B8" },
+                  }}
+                  InputProps={{
+                    style: { color: "#E2E8F0" },
+                    sx: {
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "#94A3B8",
+                      },
+                    },
+                  }}
                 />
 
                 <Button
                   sx={{
                     marginTop: "1rem",
+                    backgroundColor: "#3B82F6",
+                    color: "#E2E8F0",
+                    "&:hover": {
+                      backgroundColor: "#4B5563",
+                    },
                   }}
                   variant="contained"
                   color="primary"
                   type="submit"
                   fullWidth
-                  //   disabled={isLoading}
                 >
                   Sign Up
                 </Button>
 
-                <Typography textAlign={"center"} m={"1rem"}>
+                <Typography
+                  textAlign={"center"}
+                  m={"1rem"}
+                  sx={{ color: "#E2E8F0" }}
+                >
                   OR
                 </Typography>
 
                 <Button
-                  //   disabled={isLoading}
                   fullWidth
                   variant="text"
                   onClick={toggleLogin}
+                  sx={{ color: "#3B82F6" }}
                 >
                   Login Instead
                 </Button>
@@ -250,6 +616,6 @@ function Login() {
       </Container>
     </div>
   );
-}
+};
 
 export default Login;
