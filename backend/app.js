9216e914a339bc9ea1242aef2bd156adc68fe63c
Krishna Thakkar <work.krishnat@gmail.com>
2024-06-22 18:38:13 +0530

    backend socketio setup

--- backend/app.js
+++ backend/app.js
@@ -5,20 +5,34 @@ import { errorMiddleware } from "./middlewares/error.js";
 import chatRouter from "./routes/chat.js";
 import userRouter from "./routes/user.js";
 import { connectDB } from "./utils/features.js";
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
+import { Server } from "socket.io";
+import http from "http";
+import { corsOptions } from "./constants/config.js";
+import cors from "cors";
+import { NEW_MESSAGE, NEW_MESSAGE_ALERT } from "./constants/events.js";
+import { v4 as uuid } from "uuid";
+import { getSockets } from "./lib/helper.js";
+import { Message } from "./models/message.js";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: corsOptions,
+});
 
 const port = process.env.PORT || 8080;
 const envMode = process.env.NODE_ENV.trim() || "PRODUCTION";
+const userSocketIDs = new Map();
 
-cloudinary.config({ 
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 connectDB();
 
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,10 +41,63 @@ app.use(cookieParser());
 app.use("/user", userRouter);
 app.use("/chat", chatRouter);
 
+const user = {
+  _id: "moimsd",
+  name: "kt",
+};
+
+io.on("connection", (socket) => {
+  console.log(`Joined ${socket.id}`);
+
+  userSocketIDs.set(user._id, socket.id);
+
+  socket.on(NEW_MESSAGE, async ({ chatId, members, message }) => {
+    const messageForRealTime = {
+      content: message,
+      _id: uuid(),
+      sender: {
+        _id: user._id,
+        name: user.name,
+      },
+      chat: chatId,
+      createdAt: new Date().toISOString(),
+    };
+
+    const messageForDB = {
+      content: message,
+      sender: user._id,
+      chat: chatId,
+    };
+
+    const membersSockets = getSockets(members);
+
+    try {
+      await Message.create(messageForDB)
+    } catch (error) {
+      
+    }
+
+    io.to(membersSockets).emit(NEW_MESSAGE, {
+      chatId,
+      message: messageForRealTime,
+    });
+
+    io.to(membersSockets).emit(NEW_MESSAGE_ALERT, {chatId})
+
+
+    console.log(messageForRealTime);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+    userSocketIDs.delete(user._id.toString());
+  });
+});
+
 app.use(errorMiddleware);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server is listening on port ${port} in ${envMode} mode`);
 });
 
-export {envMode}
+export { envMode, userSocketIDs };
--- backend/lib/helper.js
+++ backend/lib/helper.js
@@ -1,5 +1,13 @@
+import { userSocketIDs } from "../app.js";
+
 export const getOtherMember = (members, userId) => {
   members.find(({ _id }) => _id.toString() != userId.toString());
+};
+
+export const getSockets = (users = []) => {
+  const sockets = users.map((user) => userSocketIDs.get(user.toString()));
+
+  return sockets;
 };
 
 export const getBase64 = (file) =>
